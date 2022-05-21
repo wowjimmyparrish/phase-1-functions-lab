a2543ad1ad8edefb1dcfefa0986cf5b8b33cf8dc
James Parrish <jthomasparrishjr@gmail.com>
2022-05-21 17:40:51 -0500

    commit tree 8926cefa2cb56ec454fb0323f7940892bb4f7a54

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    const headQuarters = 42
+    if (pickUp > headQuarters) {
+        return pickUp - headQuarters
+    }
+    if (pickUp < headQuarters) {
+        return headQuarters - pickUp
+    }
+}
+function distanceFromHqInFeet(inFeet) {
+    return distanceFromHqInBlocks(inFeet) * 264
+}
+function distanceTravelledInFeet(start, destination){
+     if (start > destination){
+        return (start - destination) * 264
+     }
+     if (start < destination){
+         return (destination - start) * 264
+     }
+}
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0
+    }
+    else if (distance > 400 && distance <= 2000){
+        return 0.02 * (distance - 400)
+    }
+    else if (distance > 2000 && distance <= 2500){
+        return 25
+    }
+    else (distance > 2500)
+        return 'cannot travel that far'
+    }
